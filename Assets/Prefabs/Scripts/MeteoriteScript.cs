2a7586842217c63740907e6090c0898d907a3705
ludwigvan <home@ludwigvans-iMac.domain.lv>
2022-06-12 21:42:55 +0300

    srgsd

--- Assets/Prefabs/Scripts/MeteoriteScript.cs
+++ Assets/Prefabs/Scripts/MeteoriteScript.cs
@@ -7,16 +7,18 @@ public class MeteoriteScript : MonoBehaviour
 
     public bool canDespawn;
     Renderer rndr;
+    public MeteoriteCounter meteoriteCounter;
 
 
 
-    // Start is called before the first frame update
+
     void Start()
     {
         rndr = GetComponent<Renderer>();
+        meteoriteCounter = FindObjectOfType<MeteoriteCounter>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (rndr.isVisible)
@@ -24,26 +26,24 @@ public class MeteoriteScript : MonoBehaviour
             canDespawn = true;
         }
 
-
-        
     }
 
-    private void OnTriggerExit2D (Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (canDespawn)
         {
             Destroy(gameObject);
             print("Meteorite destroyed");
         }
-
     }
 
-        private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.name == "Bullet(Clone)")
         {
             Destroy(gameObject);
+            meteoriteCounter.MeteoriteDown();
         }
     }
-   
+
 }
